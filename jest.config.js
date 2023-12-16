3eedcf869c164295c3f489ba5d40d5c52b91f257
Logan Luo <logan.luo@cronoslabs.org>
2023-12-16 23:40:51 +0800

    chore: update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,9 @@ module.exports = {
   testEnvironment: 'jsdom',
   modulePathIgnorePatterns: ['dist'],
   testMatch: ['<rootDir>/packages/**/*.test.[jt]s?(x)'],
+  // Set maxWorkers to 1 to circumvent the "Do not know how to serialize a BigInt" issue
+  // https://github.com/jestjs/jest/issues/11617#issuecomment-1028651059
+  maxWorkers: 1,
   moduleNameMapper: {
     '^@web3-kit/(.*)$': [
       '<rootDir>/packages/$1/src',
