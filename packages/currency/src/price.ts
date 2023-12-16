12da829ddc0d48636963041301438d90fe9fe37b
Logan Luo <logan.luo@cronoslabs.org>
2023-12-16 15:57:22 +0800

    docs: add Price docs

--- packages/currency/src/price.ts
+++ packages/currency/src/price.ts
@@ -6,17 +6,32 @@ import invariant from 'tiny-invariant';
 import type { Currency } from './currency';
 import { CurrencyAmount } from './currencyAmount';
 
+/**
+ * Subclass of Fraction. Represents a price between two currencies.
+ */
 export class Price<
   TBase extends Currency,
   TQuote extends Currency,
 > extends Fraction {
+  /**
+   * The base currency of the price.
+   */
   public readonly baseCurrency: TBase;
+
+  /**
+   * The quote currency of the price.
+   */
   public readonly quoteCurrency: TQuote;
+
+  /**
+   * The scalar value used for conversions.
+   */
   public readonly scalar: Fraction;
 
   /**
-   * Construct a price, either with the base and quote currency amount, or the
-   * @param args
+   * Constructs a new Price instance.
+   *
+   * @param args The base and quote currency amounts, or an object with baseAmount and quoteAmount properties.
    */
   public constructor(
     ...args:
@@ -56,7 +71,9 @@ export class Price<
   }
 
   /**
-   * Flip the price, switching the base and quote currency
+   * Flips the price, switching the base and quote currencies.
+   *
+   * @returns A new Price instance with the flipped currencies.
    */
   public override invert(): Price<TQuote, TBase> {
     return new Price(
@@ -68,8 +85,13 @@ export class Price<
   }
 
   /**
-   * Multiply the price by another price, returning a new price. The other price must have the same base currency as this price's quote currency
-   * @param other the other price
+   * Multiplies the price by another price, returning a new price.
+   *
+   *  The other price must have the same base currency as this price's quote currency.
+   *
+   * @param other The other price to multiply by.
+   * @returns A new Price instance representing the product.
+   * @throws 'TOKEN' if the other price's base currency does not match this price's quote currency.
    */
   public override mul<TOtherQuote extends Currency>(
     other: Price<TQuote, TOtherQuote>,
@@ -85,8 +107,11 @@ export class Price<
   }
 
   /**
-   * Return the amount of quote currency corresponding to a given amount of the base currency
-   * @param currencyAmount the amount of base currency to quote against the price
+   * Returns the amount of quote currency corresponding to a given amount of the base currency.
+   *
+   * @param currencyAmount The amount of base currency to quote against the price.
+   * @returns A new CurrencyAmount instance representing the quoted amount of quote currency.
+   * @throws 'TOKEN' if the currency amount's currency does not match this price's base currency.
    */
   public quote(currencyAmount: CurrencyAmount<TBase>): CurrencyAmount<TQuote> {
     invariant(currencyAmount.currency.eq(this.baseCurrency), 'TOKEN');
@@ -100,17 +125,46 @@ export class Price<
   }
 
   /**
-   * Get the value scaled by decimals for formatting
+   * Gets the value scaled by decimals for formatting.
+   *
    * @private
+   * @returns The adjusted Fraction value.
    */
   private get adjustedForDecimals(): Fraction {
     return super.mul(this.scalar);
   }
 
+  /**
+   * Converts the Price to a fixed-point decimal string representation.
+   *
+   * @param decimalPlaces The number of decimal places to include in the string.
+   * @param rounding The rounding mode to use.
+   * @returns The fixed-point decimal string representation of the Price.
+   */
   public override toFixed(
     decimalPlaces = 4,
     rounding?: BignumberJs.RoundingMode,
   ): string {
     return this.adjustedForDecimals.toFixed(decimalPlaces, rounding);
+  }
+
+  /**
+   * Converts the price to a formatted string representation.
+   *
+   * @param decimalPlaces The number of decimal places to include in the string.
+   * @param roundingMode The rounding mode to use.
+   * @param format The formatting options to apply.
+   * @returns The formatted string representation of the Price.
+   */
+  public override toFormat(
+    decimalPlaces = 4,
+    roundingMode?: BignumberJs.RoundingMode,
+    format?: BignumberJs.Format,
+  ): string {
+    return this.adjustedForDecimals.toFormat(
+      decimalPlaces,
+      roundingMode,
+      format,
+    );
   }
 }
