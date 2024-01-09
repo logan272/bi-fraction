0bdccc92d196d5b157c0b35d87c640fe60b44550
Logan Luo <logan.luo@cronoslabs.org>
2024-01-09 17:18:51 +0800

    feat: remove asset package and rename project

--- packages/asset/src/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './amount';
-export * from './asset';
-export * from './erc20Token';
-export * from './price';
-// re-export @fraction-asset/fraction
-export * from '@fraction-asset/fraction';
