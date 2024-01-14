bd4f430fb416e5d0b27bb71cbbbcc6c0b6bcc302
Logan Luo <logan.luo@cronoslabs.org>
2024-01-14 11:33:02 +0800

    test: add RoundingMode test cases

--- src/bn.test.ts
+++ src/bn.test.ts
@@ -1,3 +1,5 @@
+import BignumberJs from 'bignumber.js';
+
 import { Bn } from './bn';
 
 describe('bn', () => {
@@ -43,5 +45,172 @@ describe('bn', () => {
     expect(Bn('$2.5000').isNaN()).toBe(true);
     expect(Bn('2.5000 USD').isNaN()).toBe(true);
     expect(Bn('3.0000000000000005e+21').isNaN()).toBe(false);
+  });
+
+  describe('RoundingMode.', () => {
+    // (0) ROUND_UP
+    // Rounds away from zero
+    describe('RoundingMode.ROUND_UP', () => {
+      it('should pass', () => {
+        const B = Bn.clone({ ROUNDING_MODE: BignumberJs.ROUND_UP });
+        expect(B('1.4').toFixed(0)).toBe('2');
+        expect(B('1.5').toFixed(0)).toBe('2');
+        expect(B('1.6').toFixed(0)).toBe('2');
+
+        expect(B('-1.4').toFixed(0)).toBe('-2');
+        expect(B('-1.5').toFixed(0)).toBe('-2');
+        expect(B('-1.6').toFixed(0)).toBe('-2');
+      });
+    });
+
+    describe('trailing zeros', () => {
+      it('should pass', () => {
+        expect(Bn('1.40').toFixed(2)).toBe('1.40');
+      });
+    });
+
+    // (1) ROUND_DOWN
+    // Rounds towards from zero
+    describe('RoundingMode.ROUND_DOWN', () => {
+      it('should pass', () => {
+        const B = Bn.clone({ ROUNDING_MODE: BignumberJs.ROUND_DOWN });
+        expect(B('1.4').toFixed(0)).toBe('1');
+        expect(B('1.5').toFixed(0)).toBe('1');
+        expect(B('1.6').toFixed(0)).toBe('1');
+
+        expect(B('-1.4').toFixed(0)).toBe('-1');
+        expect(B('-1.5').toFixed(0)).toBe('-1');
+        expect(B('-1.6').toFixed(0)).toBe('-1');
+      });
+    });
+
+    // (2) ROUND_CEIL
+    // Rounds towards Infinity
+    describe('RoundingMode.ROUND_CEIL', () => {
+      it('should pass', () => {
+        const B = Bn.clone({ ROUNDING_MODE: BignumberJs.ROUND_CEIL });
+        expect(B('1.4').toFixed(0)).toBe('2');
+        expect(B('1.5').toFixed(0)).toBe('2');
+        expect(B('1.6').toFixed(0)).toBe('2');
+
+        expect(B('-1.4').toFixed(0)).toBe('-1');
+        expect(B('-1.5').toFixed(0)).toBe('-1');
+        expect(B('-1.6').toFixed(0)).toBe('-1');
+      });
+    });
+
+    // (3) ROUND_FLOOR
+    // Rounds towards -Infinity
+    describe('RoundingMode.ROUND_FLOOR', () => {
+      it('should pass', () => {
+        const B = Bn.clone({ ROUNDING_MODE: BignumberJs.ROUND_FLOOR });
+        expect(B('1.4').toFixed(0)).toBe('1');
+        expect(B('1.5').toFixed(0)).toBe('1');
+        expect(B('1.6').toFixed(0)).toBe('1');
+
+        expect(B('-1.4').toFixed(0)).toBe('-2');
+        expect(B('-1.5').toFixed(0)).toBe('-2');
+        expect(B('-1.6').toFixed(0)).toBe('-2');
+      });
+    });
+
+    // (4) ROUND_HALF_UP
+    // Rounds towards nearest neighbour. If equidistant, rounds away from zero
+    describe('RoundingMode.ROUND_HALF_UP', () => {
+      it('should pass', () => {
+        const B = Bn.clone({ ROUNDING_MODE: BignumberJs.ROUND_HALF_UP });
+        expect(B('1.4').toFixed(0)).toBe('1');
+        expect(B('1.5').toFixed(0)).toBe('2');
+        expect(B('1.6').toFixed(0)).toBe('2');
+
+        expect(B('-1.4').toFixed(0)).toBe('-1');
+        expect(B('-1.5').toFixed(0)).toBe('-2');
+        expect(B('-1.6').toFixed(0)).toBe('-2');
+      });
+    });
+
+    // (5) ROUND_HALF_DOWN
+    // Rounds towards nearest neighbour. If equidistant, rounds towards zero
+    describe('RoundingMode.ROUND_HALF_DOWN', () => {
+      it('should pass', () => {
+        const B = Bn.clone({ ROUNDING_MODE: BignumberJs.ROUND_HALF_DOWN });
+        expect(B('1.4').toFixed(0)).toBe('1');
+        expect(B('1.5').toFixed(0)).toBe('1');
+        expect(B('1.6').toFixed(0)).toBe('2');
+
+        expect(B('-1.4').toFixed(0)).toBe('-1');
+        expect(B('-1.5').toFixed(0)).toBe('-1');
+        expect(B('-1.6').toFixed(0)).toBe('-2');
+      });
+    });
+
+    // (6) ROUND_HALF_EVEN
+    // Rounds towards nearest neighbour. If equidistant, rounds towards even neighbour
+    describe('RoundingMode.ROUND_HALF_EVEN', () => {
+      it('should pass', () => {
+        const B = Bn.clone({ ROUNDING_MODE: BignumberJs.ROUND_HALF_EVEN });
+        expect(B('1.4').toFixed(0)).toBe('1');
+        expect(B('1.5').toFixed(0)).toBe('2');
+        expect(B('1.6').toFixed(0)).toBe('2');
+
+        expect(B('1.14').toFixed(1)).toBe('1.1');
+        expect(B('1.15').toFixed(1)).toBe('1.2');
+        expect(B('1.16').toFixed(1)).toBe('1.2');
+
+        expect(B('-1.4').toFixed(0)).toBe('-1');
+        expect(B('-1.5').toFixed(0)).toBe('-2');
+        expect(B('-1.6').toFixed(0)).toBe('-2');
+
+        expect(B('-1.14').toFixed(1)).toBe('-1.1');
+        expect(B('-1.15').toFixed(1)).toBe('-1.2');
+        expect(B('-1.16').toFixed(1)).toBe('-1.2');
+
+        expect(B('2.4').toFixed(0)).toBe('2');
+        expect(B('2.5').toFixed(0)).toBe('2');
+        expect(B('2.6').toFixed(0)).toBe('3');
+
+        expect(B('2.24').toFixed(1)).toBe('2.2');
+        expect(B('2.25').toFixed(1)).toBe('2.2');
+        expect(B('2.26').toFixed(1)).toBe('2.3');
+
+        expect(B('-2.4').toFixed(0)).toBe('-2');
+        expect(B('-2.5').toFixed(0)).toBe('-2');
+        expect(B('-2.6').toFixed(0)).toBe('-3');
+
+        expect(B('-2.24').toFixed(1)).toBe('-2.2');
+        expect(B('-2.25').toFixed(1)).toBe('-2.2');
+        expect(B('-2.26').toFixed(1)).toBe('-2.3');
+      });
+    });
+
+    // (7) ROUND_HALF_CEIL
+    // Rounds towards nearest neighbour. If equidistant, rounds towards Infinity
+    describe('RoundingMode.ROUND_HALF_CEIL', () => {
+      it('should pass', () => {
+        const B = Bn.clone({ ROUNDING_MODE: BignumberJs.ROUND_HALF_CEIL });
+        expect(B('1.4').toFixed(0)).toBe('1');
+        expect(B('1.5').toFixed(0)).toBe('2');
+        expect(B('1.6').toFixed(0)).toBe('2');
+
+        expect(B('-1.4').toFixed(0)).toBe('-1');
+        expect(B('-1.5').toFixed(0)).toBe('-1');
+        expect(B('-1.6').toFixed(0)).toBe('-2');
+      });
+    });
+
+    // (8) ROUND_HALF_FLOOR
+    // Rounds towards nearest neighbour. If equidistant, rounds towards -Infinity
+    describe('RoundingMode.ROUND_HALF_FLOOR', () => {
+      it('should pass', () => {
+        const B = Bn.clone({ ROUNDING_MODE: BignumberJs.ROUND_HALF_FLOOR });
+        expect(B('1.4').toFixed(0)).toBe('1');
+        expect(B('1.5').toFixed(0)).toBe('1');
+        expect(B('1.6').toFixed(0)).toBe('2');
+
+        expect(B('-1.4').toFixed(0)).toBe('-1');
+        expect(B('-1.5').toFixed(0)).toBe('-2');
+        expect(B('-1.6').toFixed(0)).toBe('-2');
+      });
+    });
   });
 });
