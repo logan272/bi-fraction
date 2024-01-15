0fec12ea5304ae53124afd246de770f6556d5df3
Logan Luo <logan.luo@cronoslabs.org>
2024-01-15 13:27:23 +0800

    test: update toPrecision test cases

--- src/toPrecision.ts
+++ src/toPrecision.ts
@@ -2,6 +2,14 @@ import type { RoundingMode } from './fraction';
 import { rounding } from './rounding';
 import { toFixed } from './toFixed';
 
+/**
+ * Converts a fraction represented by `numerator` and `denominator` to a string representation with the specified number of significant digits.
+ * @param numerator - The numerator of the fraction.
+ * @param denominator - The denominator of the fraction.
+ * @param significantDigits - The number of significant digits in the resulting string representation.
+ * @param roundingMode - The rounding mode to be applied.
+ * @returns The string representation of the fraction with the specified number of significant digits.
+ */
 export const toPrecision = ({
   numerator,
   denominator,
@@ -14,6 +22,7 @@ export const toPrecision = ({
   roundingMode: RoundingMode;
 }): string => {
   const isPositive = numerator * denominator >= 0n;
+  const isZero = numerator === 0n;
   const n = numerator > 0n ? numerator : -numerator;
   const d = denominator > 0n ? denominator : -denominator;
   const q = n / d; // quotient;
@@ -21,10 +30,13 @@ export const toPrecision = ({
   // the significant digit count(sdc) of the integer part
   let sdc: number = 0;
   let i = q;
+
   while (i !== 0n) {
     sdc += 1;
     i /= 10n;
   }
+
+  if (isZero) sdc = 1;
 
   if (10n ** BigInt(significantDigits) > q) {
     return toFixed({
